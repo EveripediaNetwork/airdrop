1ee9d8b5ea1df1ef711ef962e81272c9c1a120bd
Kedar Iyer <kedarmail@gmail.com>
2018-06-19 03:31:49 -0700

    airdrop looking better

--- drop.sh
+++ drop.sh
@@ -1,6 +1,6 @@
-for line in $(cat snapshot.csv); do
-	ACCOUNT=$(echo $line | tr "," "\n" | head -2 | tail -1 | tr --delete '"')
-	AMOUNT=$(echo $line | tr "," "\n" | tail -1 | tr --delete '"')
-	#IQ_AMOUNT=$(echo "$AMOUNT * 5" | bc -l | xargs printf "%0.3f" )
-	echo "$ACCOUNT,$IQ_AMOUNT"
+for line in $(head account_balances.csv); do
+	ACCOUNT=$(echo $line | tr "," "\n" | head -1)
+	AMOUNT=$(echo $line | tr "," "\n" | tail -1)
+	CURRENT_BALANCE=$(cleos get account $ACCOUNT | grep IQ)
+    echo $CURRENT_BALANCE
 done
