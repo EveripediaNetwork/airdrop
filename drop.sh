b068a2d2a6577bdf03cbf377b6a89efa0b57facb
Kedar <kedarmail@gmail.com>
2018-06-19 10:39:38 +0000

    better airdrops

--- drop.sh
+++ drop.sh
@@ -1,6 +1,25 @@
-for line in $(cat snapshot.csv); do
-	ACCOUNT=$(echo $line | tr "," "\n" | head -2 | tail -1 | tr --delete '"')
-	AMOUNT=$(echo $line | tr "," "\n" | tail -1 | tr --delete '"')
-	#IQ_AMOUNT=$(echo "$AMOUNT * 5" | bc -l | xargs printf "%0.3f" )
-	echo "$ACCOUNT,$IQ_AMOUNT"
+SYMBOL="CCTP"
+
+echo "Creating token..."
+cleos push action everipediaiq create "[\"everipediaiq\", \"100000000000.000 $SYMBOL\"]" -p everipediaiq@active
+
+ISSUANCE=$(cleos get table everipediaiq everipediaiq accounts | grep $SYMBOL)
+if [[ -z $ISSUANCE ]]; then
+    echo "Issuing..."
+    cleos push action everipediaiq issue "[\"everipediaiq\", \"10000000000.000 $SYMBOL\", \"initial supply\"]" -p everipediaiq@active
+fi
+
+for line in $(head -30 account_balances.csv); do
+    ACCOUNT=$(echo $line | tr "," "\n" | head -1)
+    AMOUNT=$(echo $line | tr "," "\n" | tail -1)
+    CURRENT_BALANCE=$(cleos get table everipediaiq $ACCOUNT accounts | grep $SYMBOL) 
+    echo $ACCOUNT
+    echo $AMOUNT
+    if [[ -z $CURRENT_BALANCE ]]; then
+        echo "Airdropping $AMOUNT $SYMBOL to $ACCOUNT"
+        cleos push action everipediaiq transfer "[\"everipediaiq\", \"$ACCOUNT\", \"$AMOUNT $SYMBOL\", \"airdrop\"]" -p everipediaiq@active
+    else
+        echo "Skipping $ACCOUNT"
+    fi 
 done
+
