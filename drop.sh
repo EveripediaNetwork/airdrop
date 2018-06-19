6cdc314a4814ae6c96792bbd48c4433b6e8f4ff4
Kedar <kedarmail@gmail.com>
2018-06-19 10:47:10 +0000

    1000 account airdrop tested

--- drop.sh
+++ drop.sh
@@ -1,20 +1,26 @@
 SYMBOL="CCTP"
 
 echo "Creating token..."
-cleos push action everipediaiq create "[\"everipediaiq\", \"100000000000.000 $SYMBOL\"]" -p everipediaiq@active
+CREATED=$(cleos get table everipediaiq $SYMBOL stat | grep $SYMBOL)
+if [[ -z $CREATED ]]; then
+    echo "Creating..."
+    cleos push action everipediaiq create "[\"everipediaiq\", \"100000000000.000 $SYMBOL\"]" -p everipediaiq@active
+else
+    echo "Token exists. Skipping create"
+fi
 
 ISSUANCE=$(cleos get table everipediaiq everipediaiq accounts | grep $SYMBOL)
 if [[ -z $ISSUANCE ]]; then
     echo "Issuing..."
     cleos push action everipediaiq issue "[\"everipediaiq\", \"10000000000.000 $SYMBOL\", \"initial supply\"]" -p everipediaiq@active
+else
+    echo "Token already issued. Skipping issue"
 fi
 
-for line in $(head -30 account_balances.csv); do
+for line in $(head -1000 account_balances.csv); do
     ACCOUNT=$(echo $line | tr "," "\n" | head -1)
     AMOUNT=$(echo $line | tr "," "\n" | tail -1)
     CURRENT_BALANCE=$(cleos get table everipediaiq $ACCOUNT accounts | grep $SYMBOL) 
-    echo $ACCOUNT
-    echo $AMOUNT
     if [[ -z $CURRENT_BALANCE ]]; then
         echo "Airdropping $AMOUNT $SYMBOL to $ACCOUNT"
         cleos push action everipediaiq transfer "[\"everipediaiq\", \"$ACCOUNT\", \"$AMOUNT $SYMBOL\", \"airdrop\"]" -p everipediaiq@active
