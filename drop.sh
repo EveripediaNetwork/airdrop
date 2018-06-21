ea3c13ad024448f62b31635c2a6ea09579b97951
Ubuntu <eos@jungle-vm.fzmg3f2edjdepjhmwmhz4kjdng.dx.internal.cloudapp.net>
2018-06-21 14:22:58 +0000

    abstract issuer

--- drop.sh
+++ drop.sh
@@ -1,29 +1,32 @@
-SYMBOL="CCTT"
+#!/bin/bash
+
+SYMBOL="DRYB"
+ISSUER="iqairdropper"
 
 echo "Creating token..."
-CREATED=$(cleos get table everipediaiq $SYMBOL stat | grep $SYMBOL)
+CREATED=$(cleos get table $ISSUER $SYMBOL stat | grep $SYMBOL)
 if [[ -z $CREATED ]]; then
     echo "Creating..."
-    cleos push action everipediaiq create "[\"everipediaiq\", \"100000000000.000 $SYMBOL\"]" -p everipediaiq@active
+    cleos push action $ISSUER create "[\"$ISSUER\", \"100000000000.000 $SYMBOL\"]" -p $ISSUER@active
 else
     echo "Token exists. Skipping create"
 fi
 
-ISSUANCE=$(cleos get table everipediaiq everipediaiq accounts | grep $SYMBOL)
+ISSUANCE=$(cleos get table $ISSUER $ISSUER accounts | grep $SYMBOL)
 if [[ -z $ISSUANCE ]]; then
     echo "Issuing..."
-    cleos push action everipediaiq issue "[\"everipediaiq\", \"10000000000.000 $SYMBOL\", \"initial supply\"]" -p everipediaiq@active
+    cleos push action $ISSUER issue "[\"$ISSUER\", \"10000000000.000 $SYMBOL\", \"initial supply\"]" -p $ISSUER@active
 else
     echo "Token already issued. Skipping issue"
 fi
 
-for line in $(tail +2 iq_snapshot.csv | head -1000); do
+for line in $(tail +2 iq_snapshot.csv); do
     ACCOUNT=$(echo $line | tr "," "\n" | head -2 | tail -1)
     AMOUNT=$(echo $line | tr "," "\n" | tail -1)
-    CURRENT_BALANCE=$(cleos get table everipediaiq $ACCOUNT accounts | grep $SYMBOL) 
+    CURRENT_BALANCE=$(cleos get table $ISSUER $ACCOUNT accounts | grep $SYMBOL) 
     if [[ -z $CURRENT_BALANCE ]]; then
         echo "Airdropping $AMOUNT $SYMBOL to $ACCOUNT"
-        cleos push action everipediaiq transfer "[\"everipediaiq\", \"$ACCOUNT\", \"$AMOUNT $SYMBOL\", \"airdrop\"]" -p everipediaiq@active
+        cleos push action $ISSUER transfer "[\"$ISSUER\", \"$ACCOUNT\", \"$AMOUNT $SYMBOL\", \"airdrop\"]" -p $ISSUER@active
     else
         echo "Skipping $ACCOUNT"
     fi 
